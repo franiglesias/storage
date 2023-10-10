8cea969f6c292b69d6c73fd57a993cdfd291411e
Fran Iglesias <franiglesias@mac.com>
2023-10-10 17:49:25 +0200

    Removing ThorCliAdapter and replacing with a simple CliAdapted

---
+++ lib/adapter/for_registering_packages/cli/cli_adapter.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class CliAdapter
+  def run(args)
+    puts(args)
+  end
+end
--- lib/adapter/for_registering_packages/cli/thor/cli_app.rb
+++
@@ -1,6 +0,0 @@
-class CliApp < Thor
-  desc "health", "To check that the application runs"
-  def health
-    puts "OK"
-  end
-end
