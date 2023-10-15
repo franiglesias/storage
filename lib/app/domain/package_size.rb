70ace8ed2939b266a640016e7102673d9a097a1d
Fran Iglesias <franiglesias@mac.com>
2023-10-15 19:23:44 +0200

    PackageSize modelled with strict

--- lib/app/domain/package_size.rb
+++ lib/app/domain/package_size.rb
@@ -1,12 +1,3 @@
 # frozen_string_literal: true
 
-class PackageSize
-  attr_reader :size
-  def initialize(size)
-    @size = size
-  end
-
-  def ==(other)
-    @size == other.size
-  end
-end
+PackageSize = Struct.new(:size)
